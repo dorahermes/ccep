f8825b3e9a95879db89b40ecee096f02a578d4c0
Jaap van der Aar <jaapvanderaar@gmail.com>
2019-03-19 14:07:04 +0100

    added a few lines

--- scripts/master_preprocessCCEP.m
+++ scripts/master_preprocessCCEP.m
@@ -46,11 +46,14 @@ data_hdr = ft_read_header(ieeg_name,'dataformat','brainvision_eeg');
 %% look at the signal from one channel
 % make a time vector t
 t = [1:size(data,2)]./data_hdr.Fs;
-figure
-plot(t,data(5,:))
-xlabel('time(s)')
-ylabel('amplitude(uV)')
-
+for i = 31
+    figure
+    plot(t,data(i,:))
+    axis([0, 1000, -4000, 4000])
+    xlabel('time(s)')
+    ylabel('amplitude(uV)')
+    
+end
 %% re-reference (if we want)
 
 %% epoch into channel X epoch X time
@@ -61,31 +64,32 @@ events_name = fullfile(dataRootPath,['sub-' sub_label],['ses-' ses_label],'ieeg'
 
 cc_events = readtable(events_name,'FileType','text','Delimiter','\t');
 
-
 % the cc_events already has a cc_events.sample
 % If I would use round(cc_events.onset*data_hdr.FS) it would give slightly
 % different numbers.
 
 epoch_length = 5; % in seconds, -2.5:2.5
+epoch_prestim = 2.5;
 
 % define the output structure
 data_epoch = zeros(size(data,1),length(cc_events.onset),round(epoch_length*data_hdr.Fs));
 
-% define starting time for each epoch/ should I do anything with this??
-tt = [1:epoch_length*data_hdr.Fs]/data_hdr.Fs - 2.5; 
+% define time vector for all epochs (for plotting and knowing when tt==0 for onset stimulation)
+tt = [1:epoch_length*data_hdr.Fs]/data_hdr.Fs - epoch_prestim; 
 
 % loop through all events and add to the output structure
 for elec = 1:size(data,1) % for all channels
-    for l = 1:length(cc_events.onset) % for all epochs
-        data_epoch(elec,l,:) = data(elec,cc_events.sample(l)-(epoch_length/2*data_hdr.Fs)+1:cc_events.sample(l)+(epoch_length/2*data_hdr.Fs));
+    for ll = 1:length(cc_events.onset) % for all epochs
+        data_epoch(elec,ll,:) = data(elec,cc_events.sample(ll)-round((epoch_prestim*data_hdr.Fs))+1:cc_events.sample(ll)+round(((epoch_length-epoch_prestim)*data_hdr.Fs)));
     end
 end
 
 %% Make figures for specific epoch
 figure
-plot(tt,squeeze(data_epoch(5,1,:)))
+plot(tt,squeeze(data_epoch(5,1:10,:)))
 xlabel('time(s)')
 ylabel('amplitude(uV)')
+
 %% Average the epochs with the same stimulation site
 
 % Now we just assume this is always 5 stimulations, should we build
@@ -93,16 +97,48 @@ ylabel('amplitude(uV)')
 % is the same?
 amount_same_stimulation = 5; 
 
-% Reshape matrix to split into every unique combination of stimulations
-% sites. Take mean of these 5 stimulations and squeeze into 3D matrix. 
-avg_epoch_stimulation = squeeze(mean(reshape(data_epoch,size(data_epoch,1),...
-    amount_same_stimulation,(size(cc_events,1)/amount_same_stimulation),size(data_epoch,3)),2));
+% get the unique number of stimulated pairs:
+[cc_stimsets,IA,IC] =  unique(cc_events.electrical_stimulation_site);
+% run through these and see how many there are
+cc_nroftimes = zeros(length(cc_stimsets),1); % number of times each pair is stimulated
+for kk = 1:length(cc_stimsets)
+    cc_nroftimes(kk) = sum(ismember(cc_events.electrical_stimulation_site,cc_stimsets{kk}));
+end
 
-% Look at averaged data of per stimulation combination. 
-figure
-plot(tt,squeeze(avg_epoch_stimulation(5,1,:)))
-xlabel('time(s)')
-% set(gca,'Ydir','reverse')
-ylabel('amplitude(uV)')
+% check whether there is consistent stimulations, if all stimulations pairs
+% are done 5 times. 
+if cc_nroftimes == 5*(ones((height(cc_events)/5),1))
+    disp('All stimulations are done 5 times')
+else
+    disp('Caution: not all stimulations are done 5 times')
+    for ll = 1:length(cc_nroftimes)
+        if cc_nroftimes(ll) ~= 5
+            xx = [cc_stimsets(ll),' is stimulated ',cc_nroftimes(ll), ' times.'];
+            disp(xx)
+        end
+    end
+end
+
+
+% max_amount_same_stimulation = max(cc_nroftimes); 
+% 
+% % display in case there are unequal stimulation numbers
+% 
+% % generate a NaN array for all stimulation pairs, NaN, because they may not
+% % all fill up and we want to be able to use nanmean, nanstd etc 
+% cc_epoch_sorted = NaN(electrodes X max aantal stimulatiepairs(5) X time);
+% % overschrijf de NaN voor aantal matches
+% 
+% % Reshape matrix to split into every unique combination of stimulations
+% % sites. Take mean of these 5 stimulations and squeeze into 3D matrix. 
+% avg_epoch_stimulation = squeeze(mean(reshape(data_epoch,size(data_epoch,1),...
+%     amount_same_stimulation,(size(cc_events,1)/amount_same_stimulation),size(data_epoch,3)),2));
+% 
+% % Look at averaged data of per stimulation combination. 
+% figure
+% plot(tt,squeeze(avg_epoch_stimulation(3,1,:)))
+% xlabel('time(s)')
+% % set(gca,'Ydir','reverse')
+% ylabel('amplitude(uV)')
 
 %%
